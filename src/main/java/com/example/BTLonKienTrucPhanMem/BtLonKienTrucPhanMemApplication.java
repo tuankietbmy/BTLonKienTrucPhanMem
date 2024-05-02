e00a2cdbee388d3564fdf3eb7a4abdc6b2883dc2
tuankietbmy <tuankietbmy@gmail.com>
2024-05-02 20:40:37 +0700

    first commit

---
+++ src/main/java/com/example/BTLonKienTrucPhanMem/BtLonKienTrucPhanMemApplication.java
@@ -0,0 +1,13 @@
+package com.example.BTLonKienTrucPhanMem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtLonKienTrucPhanMemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtLonKienTrucPhanMemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BTLonKienTrucPhanMem/BtLonKienTrucPhanMemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BTLonKienTrucPhanMem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtLonKienTrucPhanMemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
