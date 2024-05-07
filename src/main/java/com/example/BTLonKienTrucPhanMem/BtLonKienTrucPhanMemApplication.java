54c7ebfc3ca5a67228f39a4ecfe02877f0563892
tuankietbmy <tuankietbmy@gmail.com>
2024-05-07 21:51:48 +0700

    fixloi

--- src/main/java/com/example/BTLonKienTrucPhanMem/BtLonKienTrucPhanMemApplication.java
+++ src/main/java/com/example/BTLonKienTrucPhanMem/BtLonKienTrucPhanMemApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BtLonKienTrucPhanMemApplication {
+public class BtLonKienTrucPhanMemApplication{
 
 	public static void main(String[] args ) {
 		SpringApplication.run(BtLonKienTrucPhanMemApplication.class, args);
